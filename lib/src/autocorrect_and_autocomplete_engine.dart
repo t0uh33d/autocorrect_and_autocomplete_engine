b506036da37329c9550a3e1a87e4dc63994d1f60
touheed1 <touheed@codewave.com>
2022-11-20 02:50:30 +0530

    added sorting

--- lib/src/autocorrect_and_autocomplete_engine.dart
+++ lib/src/autocorrect_and_autocomplete_engine.dart
@@ -1,12 +1,24 @@
 import 'dart:collection';
 import 'dart:math';
 
+/// [TrieEngine] uses Trie Datastructure to store the data
+/// supports alphanumeric characters
+/// Each node of our Trie is represented by [TrieNode]
 class TrieEngine {
+  /// [_rootNode] is the root node of the Trie which is created whenever
+  /// an instance of [TrieEngine] is initialzed
   final TrieNode _rootNode = TrieNode();
+
+  /// HashMap [_mp] stores the occurences of a string
   final Map<String, int> _mp = {};
+
+  /// The below ordered HashMap [_priorityMp] stores the LevenshteinDistance
+  /// of the words generated using [_getLevenshteinDistance]
   final SplayTreeMap<int, List<String>> _priorityMp =
       SplayTreeMap<int, List<String>>();
 
+  /// The constuctor for [TrieEngine] which takes in an array of string
+  /// and inserts all the strings into our Trie
   TrieEngine({List<String>? src}) {
     if (src == null) return;
     for (int idx = 0; idx < src.length; idx++) {
@@ -14,18 +26,23 @@ class TrieEngine {
     }
   }
 
-  factory TrieEngine.fromList(List<String> res) {
+  /// [TrieEngine.fromList] is a factory method to create a [TrieEngine] from an array
+  /// of strings
+  factory TrieEngine.fromList(List<String> src) {
     TrieEngine t = TrieEngine();
-    for (int idx = 0; idx < res.length; idx++) {
-      t.insertWord(res[idx]);
+    for (int idx = 0; idx < src.length; idx++) {
+      t.insertWord(src[idx]);
     }
     return t;
   }
 
+  /// get all the word occurences and count present in the trie
   Map<String, int> getAllOccurences() => _mp;
 
+  /// total occurences of the given word
   int totalOccurence(String word) => _mp[word] ?? 0;
 
+  /// recursively inserts the given word character by character into the [TrieEngine]
   void _insertWord(String word, int i, TrieNode trieNode) {
     if (i == word.length) {
       trieNode.isWord = true;
@@ -39,17 +56,7 @@ class TrieEngine {
     _insertWord(word, i + 1, trieNode.c[currChar.getIndex]!);
   }
 
-  // ignore: unused_element
-  bool _searchWord(String word, int i, TrieNode trieNode) {
-    if (i == word.length) {
-      return trieNode.isWord;
-    } else if (trieNode.c[word[i].getIndex] == null) {
-      return false;
-    }
-    return _searchWord(word, i + 1, trieNode.c[word[i].getIndex]!);
-  }
-
-  // ignore: unused_element
+  /// uses dynamic programming approach to efficiently calculate the Levenshtein distance between two strings
   int _getLevenshteinDistance(String str1, String str2) {
     List<List<int>> dp = List.generate(
         str1.length + 1, (_) => List.generate(str2.length + 1, (_) => 0));
@@ -71,14 +78,8 @@ class TrieEngine {
     return dp[str1.length][str2.length];
   }
 
-  // ignore: unused_element
-  TrieNode? _getFalseNode(TrieNode trieNode, String word, int i) {
-    if (trieNode.c[i] != null) {
-      return _getFalseNode(trieNode.c[i]!, word, i + 1);
-    }
-    return trieNode.c[i];
-  }
-
+  /// auto corrects the given string
+  /// return null if a close match doesn't exist
   String? autoCorrect(String word) {
     word = word.sanitizeString;
     if (_mp.containsKey(word)) return null;
@@ -97,6 +98,7 @@ class TrieEngine {
         : null;
   }
 
+  /// returns a list of suggestions to auto correct the given word
   List<String> autoCorrectSuggestions(String word, {int count = 5}) {
     word = word.sanitizeString;
     if (_mp.containsKey(word)) return [];
@@ -121,6 +123,7 @@ class TrieEngine {
     return r;
   }
 
+  /// returns a list of suggestions to auto complete the given word
   List<String> autoCompleteSuggestions(String searchWord) {
     searchWord = searchWord.sanitizeString;
     TrieNode? trieNode = _getSearchStartNode(searchWord, 0, _rootNode);
@@ -130,11 +133,41 @@ class TrieEngine {
     return results;
   }
 
+  // ignore: unused_element
+  TrieNode? _getFalseNode(TrieNode trieNode, String word, int i) {
+    if (trieNode.c[i] != null) {
+      return _getFalseNode(trieNode.c[i]!, word, i + 1);
+    }
+    return trieNode.c[i];
+  }
+
+  /// auto completes the given string to the first suggestion in alphabetic order
   String? autoComplete(String searchWord) {
     searchWord = searchWord.sanitizeString;
     TrieNode? trieNode = _getSearchStartNode(searchWord, 0, _rootNode);
     if (trieNode == null) return null;
     return _getTopSuggestion(searchWord, trieNode, searchWord);
+  }
+
+  /// returns an alphabetically sorted list of all the words present in the engine
+  List<String> toSortedList() {
+    List<String> res = [];
+    _generateSortedList('', _rootNode, res);
+    return res;
+  }
+
+  /// recursively traverses the entire trie and adds the words to [res]
+  void _generateSortedList(
+      String currWord, TrieNode trieNode, List<String> res) {
+    if (trieNode.isWord) {
+      res.add(currWord);
+    }
+    for (int idx = 0; idx < 36; idx++) {
+      if (trieNode.c[idx] != null) {
+        _generateSortedList(
+            currWord + idx.charFromIndex, trieNode.c[idx]!, res);
+      }
+    }
   }
 
   String _getTopSuggestion(
@@ -164,6 +197,7 @@ class TrieEngine {
     }
   }
 
+  /// finds the node from where the auto completion strings should be generated
   TrieNode? _getSearchStartNode(String word, int i, TrieNode trieNode) {
     if (i == word.length) {
       return trieNode;
@@ -173,8 +207,10 @@ class TrieEngine {
     return _getSearchStartNode(word, i + 1, trieNode.c[word[i].getIndex]!);
   }
 
+  /// returns true if the word exists in the engine
   bool contains(String word) => _mp.containsKey(word.sanitizeString);
 
+  /// insert a new word into the engine
   void insertWord(String word) {
     if (_mp.containsKey(word)) {
       _mp[word] = _mp[word]! + 1;
@@ -185,11 +221,16 @@ class TrieEngine {
 }
 
 class TrieNode {
+  /// [c] is an array of possible [TrieNode] instances
+  /// If a character doen't exist at certain index, the array will contain a null value at the same
   List<TrieNode?> c = List.filled(36, null);
+
+  /// the flag [isWord] is used to check if at a given node a word gets completed
   bool isWord = false;
 }
 
 extension Ext1 on String {
+  /// [getIndex] extension on [String] maps alphanumberic characters to index for [TrieNode] array
   int get getIndex {
     if (int.tryParse(this[0]) != null) {
       return int.parse(this[0]) + 26;
@@ -198,11 +239,13 @@ extension Ext1 on String {
     }
   }
 
+  /// sanitizes the given string into a pure lowercase alphanumberic string
   String get sanitizeString =>
       replaceAll(RegExp("[^A-Za-z0-9]+"), '').toLowerCase();
 }
 
 extension Ext2 on int {
+  /// the [charFromIndex] extension on [int] converts the given [TrieNode] array index into a character
   String get charFromIndex {
     if (this > 25) {
       return String.fromCharCode((this - 26) + 48);
--- test/autocorrect_and_autocomplete_engine_test.dart
+++ test/autocorrect_and_autocomplete_engine_test.dart
@@ -4,87 +4,41 @@ import 'dart:io';
 import 'package:autocorrect_and_autocomplete_engine/autocorrect_and_autocomplete_engine.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-bool search(String word) {
-  return testData.contains(word);
-}
-
-List<String> getSuggestions(String word) {
-  return testData
-      .where((element) => element.toLowerCase().startsWith(word.toLowerCase()))
-      .toList();
-}
-
-String getFirstSuggestions(String word) {
-  return testData
-      .where((element) => element.toLowerCase().startsWith(word.toLowerCase()))
-      .first;
-}
-
-String searchWord = 'abc';
-List<String> testData =
-    File('/Users/codewave/Desktop/10k_words.txt').readAsLinesSync();
+TrieEngine trieEngine = TrieEngine.fromList(
+    File('/Users/codewave/Desktop/10k_words.txt').readAsLinesSync());
 void main() {
-  test('test 1', () {
-    TrieEngine trie = TrieEngine(src: testData);
-
-    print("\n\nSearching the word \"$searchWord\" using tries \n");
-    DateTime startTime = DateTime.now();
-    print("Result : ${trie.contains(searchWord)}");
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
+  test('auto complete', () {
+    print(trieEngine.autoComplete("marv"));
   });
 
-  test('test 1', () {
-    print("\n\nSearching the word \"$searchWord\" using built in contains");
-    DateTime startTime = DateTime.now();
-    print("Result : ${search(searchWord)}");
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
+  test('auto complete suggestions', () {
+    print(trieEngine.autoCompleteSuggestions('mar'));
   });
 
-  // suggestion time
-  test('test 2', () {
-    TrieEngine trie = TrieEngine(src: testData);
-    print(
-        "\n\nSuggestions for words starting with \"$searchWord\" using tries \n");
-    DateTime startTime = DateTime.now();
-    print("Result : ${trie.autoComplete(searchWord)}");
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
+  test('auto correct', () {
+    print(trieEngine.autoCorrect('marve'));
   });
 
-  test('test 2', () {
-    print(
-        'Suggestions for words starting with "$searchWord" using built in startsWith');
-    DateTime startTime = DateTime.now();
-    print("Result : ${getSuggestions(searchWord)}");
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
+  test('auto correct suggestions', () {
+    print(trieEngine.autoCorrectSuggestions('helikopta', count: 10));
   });
 
-  test('test 3', () {
-    TrieEngine trie = TrieEngine(src: testData);
-    print("\ntop for word starting with \"$searchWord\" using tries \n");
-    DateTime startTime = DateTime.now();
-    print("Result : ${trie.autoComplete(searchWord) ?? 'No suggestion found'}");
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
-  });
-
-  test('test 3', () {
-    print(
-        'Suggestions for words starting with "$searchWord" using built in startsWith');
-    DateTime startTime = DateTime.now();
-    print("Result : ${getFirstSuggestions(searchWord)}");
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
-  });
-
-  test('test 4', () {
-    TrieEngine trie = TrieEngine(src: testData);
-    DateTime startTime = DateTime.now();
-    print(trie.autoCorrectSuggestions('helicoptar', count: 6));
-    print(
-        "Time taken : ${DateTime.now().difference(startTime).inMicroseconds} microseconds");
+  test('sorting', () {
+    // correct order of names
+    List<String> arr = [
+      "Acetobacter aurantius",
+      "Acinetobacter baumannii",
+      "Actinomyces israelii",
+      "Agrobacterium radiobacter",
+      "Agrobacterium tumefaciens",
+      "Anaplasma",
+      "Anaplasma phagocytophilum",
+      "Azorhizobium caulinodans",
+      "Azotobacter vinelandii"
+    ];
+    String sortedStr = arr.join().replaceAll(' ', '').toLowerCase();
+    arr.shuffle();
+    TrieEngine trieEngine = TrieEngine.fromList(arr);
+    expect(trieEngine.toSortedList().join(), sortedStr);
   });
 }
